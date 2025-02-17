00b0b90025267799c60542111b67ec3d68d949fa
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-02-17 18:42:58 +0300

    Initial commit

---
+++ app/src/main/java/com/ideasapp/petemotions/presentation/activity/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ideasapp.petemotions.presentation.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.ideasapp.petemotions.presentation.ui.theme.PetEmotionsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PetEmotionsTheme {
+
+            }
+        }
+    }
+}
+
