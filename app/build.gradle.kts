c300b3c099186f0160ad806c12cf87ba9d69b8a0
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-02-20 14:19:41 +0300

    hilt implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
+    id("kotlin-kapt")
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -44,6 +46,9 @@ android {
             excludes+="/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    kapt {
+        correctErrorTypes = true
+    }
 }
 
 dependencies {
@@ -62,7 +67,8 @@ dependencies {
     implementation(libs.androidx.material)
 
     //Hilt
-
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
 
     //tests
     testImplementation(libs.junit)
---
+++ app/src/main/java/com/ideasapp/petemotions/data/db/CalendarListDao.kt
@@ -0,0 +1,4 @@
+package com.ideasapp.petemotions.data.db
+
+
+interface CalendarListDao {}
