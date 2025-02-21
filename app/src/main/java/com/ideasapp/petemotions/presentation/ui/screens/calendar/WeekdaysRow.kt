471ff70322718c5ffb2e94b3ee22e83c8c0b070a
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-02-21 23:26:42 +0300

    WeekdaysRow

--- app/src/main/java/com/ideasapp/petemotions/presentation/ui/screens/calendar/WeekdaysRow.kt
+++ app/src/main/java/com/ideasapp/petemotions/presentation/ui/screens/calendar/WeekdaysRow.kt
@@ -1,7 +1,6 @@
 package com.ideasapp.petemotions.presentation.ui.screens.calendar
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,18 +11,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-//TODO
-// 3. week days
-// 4. calendar will take info from phone calendar
 @Composable
-fun CalendarScreen() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        TopButtonCalendarBar()
-        MonthDropDownMenu()
-        WeekdaysRow()
+fun WeekdaysRow() {
+    Row(modifier=Modifier.fillMaxWidth().fillMaxHeight(0.1f),verticalAlignment=Alignment.CenterVertically,horizontalArrangement=Arrangement.Center) {
+        Text("mon",Modifier.padding(10.dp))
+        Text("tue",Modifier.padding(10.dp))
+        Text("wed",Modifier.padding(10.dp))
+        Text("thu",Modifier.padding(10.dp))
+        Text("fri",Modifier.padding(10.dp))
+        Text("sat",Modifier.padding(10.dp))
+        Text("sun",Modifier.padding(10.dp))
     }
 }
-
-
-
-
