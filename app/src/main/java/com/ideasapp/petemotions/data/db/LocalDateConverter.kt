1ddf94d67fd29feaf19a312dd5774d946d41ea07
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-03-24 22:57:27 +0300

    db added, error with pets

--- app/src/main/java/com/ideasapp/petemotions/data/db/LocalDateConverter.kt
+++ app/src/main/java/com/ideasapp/petemotions/data/db/LocalDateConverter.kt
@@ -1,5 +1,6 @@
 package com.ideasapp.petemotions.data.db
 
+import androidx.compose.material.icons.Icons
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.room.TypeConverter
 import java.time.LocalDate
@@ -9,11 +10,13 @@ class ImageVectorConverter {
 
     @TypeConverter
     fun fromImageVector(date: ImageVector?): String? {
-        return
+        return date?.let { it.name.split(".")[1] }
     }
 
     @TypeConverter
-    fun toImageVector(dateString: String?): ImageVector? {
-        return
+    fun toImageVector(dateString: String?): ImageVector {
+        val cl = Class.forName("androidx.compose.material.icons.filled.${dateString}Kt")
+        val method = cl.declaredMethods.first()
+        return method.invoke(null, Icons.Filled) as ImageVector
     }
 }
