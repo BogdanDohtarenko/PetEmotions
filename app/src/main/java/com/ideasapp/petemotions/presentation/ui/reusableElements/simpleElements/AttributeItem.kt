2cd363daeb4f5b3e7f0f69afa1cf9184689ccc8d
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-04-09 17:58:26 +0300

    // 85. choose state isnt correctly changing  // 76. day attributes list add to day item info !!!! this tasks performed

--- app/src/main/java/com/ideasapp/petemotions/presentation/ui/reusableElements/simpleElements/AttributeItem.kt
+++ app/src/main/java/com/ideasapp/petemotions/presentation/ui/reusableElements/simpleElements/AttributeItem.kt
@@ -1,18 +1,24 @@
 package com.ideasapp.petemotions.presentation.ui.reusableElements.simpleElements
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import com.ideasapp.petemotions.presentation.ui.theme.MainTheme
 
 
 @Composable
@@ -20,16 +26,20 @@ fun AttributeItem(
     imageVector: ImageVector,
     textColor: Color,
     title: String,
-    onAttributeChooseClick: (String) -> Unit,
+    isChosen: Boolean,
     modifier: Modifier = Modifier
 ) {
     Column(
         modifier = modifier
+            .clip(RoundedCornerShape(10.dp))
+            .background(
+                if (!isChosen)
+                    MainTheme.colors.spareContentColor.copy(alpha = 0f)
+                else
+                    MainTheme.colors.mainColor
+            )
             .padding(8.dp)
-            .wrapContentSize()
-            .clickable {
-                onAttributeChooseClick(title)
-            },
+            .wrapContentSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Icon(
--- app/src/main/java/com/ideasapp/petemotions/presentation/ui/screens/calendar/ChooseDayAttributesBox.kt
+++ app/src/main/java/com/ideasapp/petemotions/presentation/ui/screens/calendar/ChooseDayAttributesBox.kt
@@ -1,11 +1,8 @@
 package com.ideasapp.petemotions.presentation.ui.screens.calendar
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,26 +13,22 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.ideasapp.petemotions.domain.entity.calendar.DayAttribute
 import com.ideasapp.petemotions.presentation.ui.reusableElements.simpleElements.AttributeItem
-import com.ideasapp.petemotions.presentation.ui.theme.MainTheme
 
 @Composable
 fun ChooseDayAttributesBox(
     textColor: Color,
-    chosenAttributeState: MutableState<Boolean>,
-    addAttributeState: MutableState<Boolean>,
+    editAttributeState: MutableState<Boolean>,
     dayAttributesList: List<DayAttribute>,
-    onAttributeChooseClick: (String) -> Unit,
+    chosenAttributesList: List<String>,
+    onChooseAttributeClick: (String) -> Unit,
     modifier : Modifier = Modifier
 ) {
     LazyVerticalGrid(
@@ -52,13 +45,10 @@ fun ChooseDayAttributesBox(
                 imageVector = attribute.imageVector,
                 textColor = textColor,
                 title = attribute.title,
-                onAttributeChooseClick = onAttributeChooseClick,
-                modifier = Modifier.background(
-                    if (chosenAttributeState.value)
-                        MainTheme.colors.mainColor
-                    else
-                        MainTheme.colors.spareContentColor
-                )
+                isChosen = chosenAttributesList.contains(attribute.title),
+                modifier = Modifier.clickable {
+                    onChooseAttributeClick(attribute.title)
+                }
             )
         }
     }
@@ -74,7 +64,7 @@ fun ChooseDayAttributesBox(
             contentDescription = "settings",
             modifier = Modifier
                 .size(40.dp)
-                .clickable { addAttributeState.value = true }
+                .clickable {editAttributeState.value = true}
         )
     }
 }
