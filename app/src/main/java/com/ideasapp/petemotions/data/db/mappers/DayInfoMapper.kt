d19fa2af999d040e29ee4f89c41a93b530457a6e
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-05-28 11:14:19 +0300

    added text field to day info

--- app/src/main/java/com/ideasapp/petemotions/data/db/mappers/DayInfoMapper.kt
+++ app/src/main/java/com/ideasapp/petemotions/data/db/mappers/DayInfoMapper.kt
@@ -5,9 +5,9 @@ import com.ideasapp.petemotions.domain.entity.calendar.DayItemInfo
 
 object DayInfoMapper {
     fun dbModelToEntity(dbModel: DayItemInfoDbModel): DayItemInfo {
-        return DayItemInfo(date = dbModel.date, mood = dbModel?.mood, petId = dbModel.petId, attributeNames = dbModel.attributeNames)
+        return DayItemInfo(date = dbModel.date, mood = dbModel.mood, petId = dbModel.petId, attributeNames = dbModel.attributeNames, text = dbModel.text)
     }
     fun entityToDbModel(entity:DayItemInfo): DayItemInfoDbModel {
-        return DayItemInfoDbModel(date = entity.date, mood = entity?.mood, petId = entity.petId, attributeNames = entity.attributeNames)
+        return DayItemInfoDbModel(date = entity.date, mood = entity.mood, petId = entity.petId, attributeNames = entity.attributeNames, text = entity.text)
     }
 }
